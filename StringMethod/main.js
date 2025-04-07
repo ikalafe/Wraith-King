03a9b14c58574ec548a46f88675ecf8338a66c59
ikalafe <imkalafe@gmail.com>
2025-04-06 20:16:55 -0400

    create folder for methods string and create file

---
+++ StringMethod/main.js
@@ -0,0 +1,17 @@
+const text = "I Love Kali Linux"
+
+// console.log(text.charAt(2))
+// console.log(text.charCodeAt(0));
+// console.log(text + " And BlackArch");
+// console.log(text.concat(" And BlackArch"));
+
+// console.log(text.trim());
+// console.log(text.trimStart());
+// console.log(text.trimEnd());
+
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+// console.log(text.includes("love"));
+// console.log(text.search("Kali"));
+console.log(text.indexOf("Kali"));
